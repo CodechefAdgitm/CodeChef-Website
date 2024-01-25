6d6be9df747a4223b3dbab33f97fe90131d8cd1b
AnishHazra <anishhazra108@gmail.com>
2024-01-25 21:01:45 +0530

    convert into main page navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,21 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div>
-      Navbar
+      <header>
+    <div class="logo">
+      <img src="C:\Users\user\Downloads\WhatsApp Image 2024-01-25 at 3.55.20 PM.jpeg" alt="Logo"/>
+    </div>
+    <nav>
+      <ul>
+       <li><a href="#"  rel="noopener noreferrer">Home</a></li>
+       <li><a href="/About"  rel="noopener noreferrer">About Us</a></li>
+       <li><a href="#"  rel="noopener noreferrer">Our Team</a></li>
+       <li><a href="#"  rel="noopener noreferrer">Department</a></li>
+       <li><a href="#"  rel="noopener noreferrer">Events</a></li>
+        <li><a href="#"  rel="noopener noreferrer">Contact US</a></li>
+      </ul>
+    </nav>
+  </header>
     </div>
   )
 }
