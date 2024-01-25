a86a7aa8d73a9cc47bae571d3223d3d03db8ecde
Mohd Haroon <haroonsaifi2001@gmail.com>
2024-01-25 18:43:22 +0530

    complete about page and fixes #3

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Navbar from "../src/Components/Navbar/Navbar.jsx";
 import Home from "../src/Components/Home/Home.jsx";
 import About from "../src/Components/About/About.jsx";
 import Team from "../src/Components/Team/Team.jsx";
-import Department from "../src/components/Department/Department.jsx";
-import Events from "../src/components/Events/Events.jsx";
-import Contact from "../src/components/Contact/Contact.jsx";
+import Department from "../src/Components/Department/Department.jsx";
+import Events from "../src/Components/Events/Events.jsx";
+import Contact from "../src/Components/Contact/Contact.jsx";
 import Footer from "../src/Components/Footer/Footer.jsx";
 
 
